40cceb909f0033c6964432983a576d951d6d409e
Mickhopes <mickael.turnel@gmail.com>
2016-04-01 10:22:03 +0200

    Création du projet Maven

---
+++ Projet/cle-valeur/src/main/java/fr/uga/info/App.java
@@ -0,0 +1,13 @@
+package fr.uga.info;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
