bcc86f64f4b6ad9b58b8695d3f3c226aa506d940
Aurelien <aurelm38@gmail.com>
2016-04-15 11:34:48 +0200

    Ajout de classes pour le réseau coté serveur
    Serveur avec le main
    AcceptConnexion pour paralleliser differents clients
    Execution requete pour resoudre la requete client
    ChatClientServeur pour le dialogue entre le client et le serveur
    Notemment la reception de requetes.

---
+++ serveur/src/main/java/fr/uga/info/ExecutionRequete.java
@@ -0,0 +1,15 @@
+package fr.uga.info;
+
+public class ExecutionRequete implements Runnable {
+	private String requeteToExecute;
+	
+	public ExecutionRequete (String requete) {
+		requeteToExecute = requete;
+	}
+
+
+	public void run() {
+		
+		
+	}
+}
---
+++ serveur/src/main/java/fr/uga/info/Serveur.java
@@ -0,0 +1,34 @@
+package fr.uga.info;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Serveur {
+	
+	public static ServerSocket ss = null;
+	public static Thread t;
+	
+	public static void main(String[] args) {
+		try {
+			ss = new ServerSocket(4242);
+		} catch (IOException e) {
+			System.out.println("Erreur dans la creation de la socket (Serveur)");
+		}
+		
+		System.out.println("------------------------------------------------------");
+		System.out.println("|                                                    |");
+		System.out.println("|                                                    |");
+		System.out.println("|                Projet TAGL - M1 Info               |");
+		System.out.println("|                Aurelien Monnet-Paquet              |");
+		System.out.println("|                Line Pouvaret                       |");
+		System.out.println("|                Antoine Thebaud                     |");
+		System.out.println("|                Mickael Turnel                      |");
+		System.out.println("|                                                    |");
+		System.out.println("|                                                    |");
+		System.out.println("------------------------------------------------------");
+		System.out.println("Le serveur est à l'écoute du port "+ss.getLocalPort());
+		
+		t = new Thread(new AccepterConnexion(ss));
+		t.start();
+	}
+}
