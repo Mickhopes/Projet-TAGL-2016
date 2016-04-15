3c3e7cfce27fd835897b17e963a5c2a9a4bf519f
Mickhopes <mickael.turnel@gmail.com>
2016-04-15 10:38:17 +0200

    Ajout de la branche stockage

---
+++ stockage/src/main/java/fr/uga/info/Stockage.java
@@ -0,0 +1,18 @@
+package fr.uga.info;
+
+import java.util.HashMap;
+
+/**
+ * 
+ * 
+ * @author Mickaël Turnel
+ */
+public class Stockage {
+	private HashMap<String, Object> map;
+	
+	public Stockage() {
+		map = new HashMap<>();
+	}
+	
+	
+}
